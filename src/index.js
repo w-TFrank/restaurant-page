b8a3806cf85bbf38834ee72600d7262dadf5706d
TFrank <madmonsterHI@gmail.com>
2023-08-16 14:03:53 -0400

    header done

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import Logo from "./frank.png";
 const content = document.querySelector("#content");
 const restaurantName = document.createElement("h1");
 const blurb = document.createElement("h4");
+const header = document.createElement("div");
+header.className = "header";
 restaurantName.textContent = "Franklin's Franks";
+restaurantName.className = "name";
 blurb.textContent = "Best dogs this side of the Mississippi!";
 const logo = new Image();
 logo.src = Logo;
-logo.id = "logo";
-content.appendChild(restaurantName);
-content.appendChild(logo);
-content.appendChild(blurb);
+logo.className = "logo";
+blurb.className = "blurb";
+content.appendChild(header);
+header.appendChild(restaurantName);
+restaurantName.appendChild(logo);
+header.appendChild(blurb);
 
 /**
  * put this code ^ 
